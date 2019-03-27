c77e12f5bb03a337040fdcbd501081dafa818e2e
Monetthigpen <monet.thigpen@gmail.com>
2019-03-27 00:30:46 -0400

    port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3050;
+var PORT = process.env.PORT || 3050;
 
 // Initialize Express
 var app = express();
